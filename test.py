dc778565291e550556fb3fb09fdaf074cd6feb2b
skelly <skelly@skell.com>
2024-03-27 15:41:18 +0200

    Fixed handle_prompt_validation, used list of dictionaries

--- test.py
+++ test.py
@@ -1,28 +1,42 @@
-def handle_prompt_validation(prompts: dict[str, any], default_values: list, validation_funcs: list[callable]) -> dict[str, any]:
-    results = {}
-    for prompt, default, validation_func in zip(prompts, default_values, validation_funcs):
+from wget_exploit import check_interface, logger, RED, GREEN, CYAN, BOLD, RESET
+import os
+
+def handle_prompt_validation(prompts: list[dict]) -> list:
+    results = []
+    for prompt in prompts:
         while True:
-            value = input(prompts[prompt]).strip()
-            value = value if value else default
-            if validation_func(value):
-                results[prompt] = value
+            value = input(prompt["text"]).strip()
+            value = value if value else prompt["default"]
+            if prompt["check"](value):
+                results.append(value)
                 break
-            # else:
-            #     logger.error(f"{RED}Invalid input. Please provide valid data.")
+            logger.error(f"{RED}Invalid input. Please provide valid data.{RESET}")
     return results
 
-
 prompts = [
-    f"{BOLD}Network interface or IP address to host the HTTP server {CYAN}{BOLD}(default: eth0):{RESET} ",
-    f"{BOLD}Specify alternate port {CYAN}{BOLD}(default: 8000):{RESET} ",
-    f"{BOLD}File to config {CYAN}{BOLD}(Must be a valid file!):{RESET} ",
-    f"{BOLD}Payload to execute (Options: {CYAN}{BOLD}forceroot, dnspoof):{RESET} ",
-    ]
-defaults = ["eth0", 8000, None]
-validation_funcs = [
-    lambda x: True,
-    lambda x: x.isdigit(),
-    lambda x: os.path.isfile(x),
-    lambda x: x in ["forceroot", "dnspoof"]
+    {
+        "text": f"{BOLD}Network interface or IP address to host the HTTP server {CYAN}{BOLD}(default: eth0):{RESET} ",
+        "default": "eth0",
+        "check": lambda x: check_interface(x)
+    },
+    {
+        "text": f"{BOLD}Specify alternate port {CYAN}{BOLD}(default: 8000):{RESET} ",
+        "default": 8000,
+        "check": lambda x: str(x).isdigit()
+    },
+    {
+        "text": f"{BOLD}File to config {CYAN}{BOLD}(Must be a valid file!):{RESET} ",
+        "default": "",
+        "check": lambda x: x != "" and os.path.isfile(x)
+    },
+    {
+        "text": f"{BOLD}Payload to execute (Options: {CYAN}{BOLD}forceroot, dnspoof):{RESET} ",
+        "default": "",
+        "check": lambda x: x in ["forceroot", "dnspoof"]
+    }
 ]
-results = handle_prompt_validation(prompts, defaults, validation_funcs)
+
+results = handle_prompt_validation(prompts)
+serve_host, port, file, payload = results
+
+print(results)
